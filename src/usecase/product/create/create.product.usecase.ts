4ad3b115df39a647efe1a6c21eaae9c810429b68
Gustavo Figueiredo <gustavo.figueiredo@fast2mine.com>
2023-01-13 08:09:45 -0300

    feat: create CreateProductUseCase

---
+++ src/usecase/product/create/create.product.usecase.ts
@@ -0,0 +1,22 @@
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+import {
+  InputCreateProductDTO,
+  OutputCreateProductDTO,
+} from "./create.product.dto";
+
+export default class CreateProductUseCase {
+
+  constructor(private productRepository: ProductRepositoryInterface) {}
+
+  async execute(input: InputCreateProductDTO): Promise<OutputCreateProductDTO> {
+    const product = ProductFactory.create(input.type || "a", input.name, input.price);
+
+    await this.productRepository.create(product);
+    return {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+    };
+  }
+}
